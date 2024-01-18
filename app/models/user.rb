f9b72e8deee5528d21f0a87525aa4b4cc51f66e9
Apple <kisin1010@gmail.com>
2024-01-18 21:03:23 +0900

    【ER図に書いた各種テーブルとmodelの作成】

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
   include DeviseTokenAuth::Concerns::User
 end
---
+++ db/migrate/20240115123718_create_article_likes.rb
@@ -0,0 +1,11 @@
+class CreateArticleLikes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :article_likes do |t|
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+      t.references :article, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
