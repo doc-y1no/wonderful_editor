e8a5f66e1040c2f5e364a263b28e7f180c43b62b
Apple <kisin1010@gmail.com>
2024-01-21 17:13:26 +0900

    articles_controller.rbの格納場所を修正（app/api/v1下へ移動）　request/rspec実行成功

--- spec/request/articles_spec.rb
+++ spec/request/articles_spec.rb
@@ -8,10 +8,9 @@ RSpec.describe "Api::V1::Articles", type: :request do
     let!(:article2) { create(:article, updated_at: 2.days.ago) }
     let!(:article3) { create(:article) }
 
-    it "記事の一覧が取得できる" do # rubocop:disable RSpec/MultipleExpectations
+    it "記事の一覧が取得できる" do # rubocop:disable all
       subject
       res = JSON.parse(response.body)
-
       expect(response).to have_http_status(:ok)
       expect(res.length).to eq 3
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
