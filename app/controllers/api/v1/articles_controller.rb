af5df333177e33d98c707d3ef60292c8a889b407
Apple <kisin1010@gmail.com>
2024-01-22 22:59:25 +0900

    【記事作成の実装】

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -4,5 +4,15 @@ module Api::V1
       articles = Article.order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
     end
+
+    def show
+      render json: article_path, serializer: Api::V1::ArticleSerializer
+    end
+
+    private
+
+      def article_path
+        @article = Article.find(params[:id])
+      end
   end
 end
