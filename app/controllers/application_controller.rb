8b0c5be541108f5ef9fa23dea26c8457b8a3d0f4
Apple <kisin1010@gmail.com>
2024-02-03 18:13:01 +0900

    【RSpec UPDATEテスト】

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_forgery_protection
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   extend Devise::Models
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
-  include DeviseTokenAuth::Concerns::User
+
   validates :name, presence: true
 end
