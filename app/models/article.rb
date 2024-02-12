f1f2ecd5d4193435c486c5807c433227e8005d7d
Apple <kisin1010@gmail.com>
2024-02-12 17:23:39 +0900

    【 Article model に下書き機能を追加【下書き機能の実装】】

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :article_likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  validates :title, presence: true
+
+  enum status: { draft: "draft", published: "published" }
 end
